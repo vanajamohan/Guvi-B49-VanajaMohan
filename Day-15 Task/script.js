962e24a72d768d47be893b7d7f0559f2e578df47
Vana <vanajamohan771997@gmail.com>
2023-07-21 21:52:30 +0530

    Day-15 Task

---
+++ Day-15 Task/script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("myform");
+const table = document.getElementById("dataTable");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  debugger;
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const address = document.getElementById("address").value;
+  const pincode = document.getElementById("pincode").value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const foodcheckboxes = document.querySelectorAll(
+    'input[name="food"]:checked'
+  );
+  const foodChoices = Array.from(foodcheckboxes).map(
+    (checkbox) => checkbox.value
+  );
+
+  if (foodChoices.length < 2) {
+    alert("Please choose at least 2 food options.");
+    return;
+  }
+
+  const newRow = table.insertRow(); //document.createElement("tr");
+  const fnameCol = newRow.insertCell(); 
+  const lnameCol = newRow.insertCell();
+  const addCol = newRow.insertCell();
+  const pinCol = newRow.insertCell();//document.createElement("td");
+  const genderCol = newRow.insertCell(); //document.createElement("td");
+  const foodCol = newRow.insertCell(); //document.createElement("td");
+
+  fnameCol.textContent = fname;
+  lnameCol.textContent = lname;
+  addCol.textContent = address;
+  pinCol.textContent = pincode;
+  genderCol.textContent = gender;
+  foodCol.textContent = foodChoices.join(", ");
+
+  form.reset();
+});
