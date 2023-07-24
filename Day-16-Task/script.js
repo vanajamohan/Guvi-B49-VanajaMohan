c5daeada0635df03ef4a222a81f5cad361b9e928
Vana <vanajamohan771997@gmail.com>
2023-07-24 22:26:51 +0530

    Day 16 Task

---
+++ Day-16-Task/script.js
@@ -0,0 +1,53 @@
+
+
+
+const ele = document.getElementById("display");
+setTimeout(function () {
+  ele.innerText="Count Down starts!!!!";
+
+        setTimeout(() => {
+            
+            ele.innerText="10";
+
+            setTimeout(() => {
+                ele.innerText="9";
+
+                setTimeout(() => {
+                    ele.innerText="8";
+
+                        setTimeout(() => {
+                            ele.innerText="7";
+
+                                setTimeout(() => {
+                                     ele.innerText="6";
+
+                                        setTimeout(() => {
+                                            ele.innerText="5";
+                                                            
+                                            setTimeout(() => {
+                                                ele.innerText="4";
+                                                               
+                                                setTimeout(() => {
+                                                    ele.innerText="3";
+                                                                    
+                                                    setTimeout(() => {
+                                                        ele.innerText="2";
+                                                             
+                                                        setTimeout(() => {
+                                                            ele.innerText="1";
+
+                                                             setTimeout(() => {
+                                                            
+                                                            ele.innerText="Happy Independence Day to All !!!!!!!";
+                                                    }, 1000);
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+}, 1000);
